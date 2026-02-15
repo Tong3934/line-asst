bb486f7aa1771269b29d0497b55dbe9695996acb
Treenares <38903392+Tong3934@users.noreply.github.com>
2026-02-15 15:29:48 +0700

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
 set -e
 
-# 📥 clone repo ครั้งแรก
 if [ ! -d "/app/.git" ]; then
   echo "📥 First time clone..."
   git clone -b ${BRANCH} ${REPO_URL} /app
-
   echo "📦 Installing dependencies (first time)..."
   pip install --no-cache-dir -r /app/requirements.txt
 else
   echo "🔄 Repo exists, pulling latest..."
-  cd /app
-  git pull
+  cd /app && git pull
 fi
 
-cd /app
-
-echo "🚀 Starting ngrok..."
-python start_ngrok.py &
-
-# รอ tunnel ขึ้นก่อน
-sleep 3
-
 echo "🚀 Starting FastAPI..."
-exec python main.py
+exec python /app/main.py
